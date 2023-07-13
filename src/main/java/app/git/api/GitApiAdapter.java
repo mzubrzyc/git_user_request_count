1f87ce4ad12cfbf32825fd0eb930cb0522663227
mzubrzyc <mzubrzyc>
2023-07-13 20:58:30 +0200

    moved git_base_url to properties file

--- src/main/java/app/git/api/GitApiAdapter.java
+++ src/main/java/app/git/api/GitApiAdapter.java
@@ -1,11 +1,9 @@
 package app.git.api;
 
-
 import app.git.common.Login;
 import app.git.login.GitApiPort;
 import app.git.login.UserGitInfo;
 import app.git.util.UserGitInfoMapper;
-
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
@@ -16,26 +14,30 @@ public class GitApiAdapter implements GitApiPort {
 
     private final HttpClient httpClient = HttpClient.newHttpClient();
     private final UserGitInfoMapper userGitInfoMapper = UserGitInfoMapper.getInstance();
-    private static final String BASE_GIT_API_URL = "https://api.github.com";
+    private final String BASE_GIT_API_URL;
+
+    public GitApiAdapter(String baseGitApiUrl) {
+        this.BASE_GIT_API_URL = baseGitApiUrl;
+    }
 
     @Override
     public UserGitInfo getUserGitInfoForLogin(Login login) throws IOException, InterruptedException {
         String userGitInfoJson = httpClient.send(
-                prepareHttpRequest(login.value()),
-                HttpResponse.BodyHandlers.ofString()
+            prepareHttpRequest(login.value()),
+            HttpResponse.BodyHandlers.ofString()
         ).body();
         return userGitInfoMapper.toUserGitInfoRequest(userGitInfoJson, login);
     }
 
     private HttpRequest prepareHttpRequest(String login) {
-        String baseGitUrl = "https://api.github.com/users/";
+        String baseGitUrl = BASE_GIT_API_URL + "users/";
         return HttpRequest.newBuilder()
-                .uri(URI.create(baseGitUrl.concat(login)))
-                .GET()
-                .build();
+                          .uri(URI.create(baseGitUrl.concat(login)))
+                          .GET()
+                          .build();
     }
 
     /*
-    * todo: create utility to build URI
-    * */
+     * todo: create utility to build URI
+     * */
 }
--- src/main/java/app/git/api/GitApiConfig.java
+++ src/main/java/app/git/api/GitApiConfig.java
@@ -1,14 +1,23 @@
 package app.git.api;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class GitApiConfig {
 
+    private final String BASE_GIT_API_URL;
+
+    public GitApiConfig(
+        @Value("${base.git.api.url}") String baseGitApiUrl
+    ) {
+        this.BASE_GIT_API_URL = baseGitApiUrl;
+    }
+
     @Bean
     public GitApiAdapter gitApiAdapter() {
-        return new GitApiAdapter();
+        return new GitApiAdapter(BASE_GIT_API_URL);
     }
 
 }
